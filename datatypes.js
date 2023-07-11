ea2c22df5c0c957912ef9707c379b05b4a468603
bitsonymousm <bitsonymous@gmail.com>
2023-07-12 05:24:37 +0530

    new girhubs

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // new standard
+// alert("hello")  // not for node js only for browser
+
+console.log(3+3)
+console.log("Himanshu")
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone 
+// undefined =>
+// symbol => unique
+
+
+
+// object
+let un = undefined;
+let age = 54
+console.log(typeof("Himanshu"))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(un))
---
+++ var.js
@@ -0,0 +1,4 @@
+const idps = 2165464;
+// let password = "himanshu"
+let mail = "ihaufi@gmail.com"
+console.table([idps.password,mail]);
